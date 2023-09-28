93b6ec8ac493f59f8f96da12a69c1788c1d016e7
juanlamadrid20 <juan.lamadrid@gmail.com>
2023-09-27 20:50:45 -0400

    streamlit 2 dash

--- app.py
+++ app.py
@@ -1,57 +1,56 @@
-import time
-import streamlit as st
-from utils import load_chain
+import dash
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output, State
+from utils import load_chain  # Loading your custom function
+from flask import Flask
 
-# Custom image for the app icon and the assistant's avatar
 company_logo = 'https://www.app.nl/wp-content/uploads/2019/01/Blendle.png'
+chain = load_chain()
+messages = [{"role": "assistant", "content": "Hi human! I am your smart AI. How can I help you today?"}]
 
-# Configure Streamlit page
-st.set_page_config(
-    page_title="Your Notion Chatbot",
-    page_icon=company_logo
+server = Flask(__name__)
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP], server=server)
+
+app.layout = dbc.Container(
+    fluid=True,
+    children=[
+        dbc.Row(dbc.Col(dbc.Card(dbc.CardBody("Your Notion Chatbot"), color="primary"))),
+        dbc.Row(dbc.Col(dbc.Card(
+            dbc.CardBody(
+                [dbc.ListGroupItem(message["content"], color="light", className=message["role"]) for message in messages],
+                id='messages',
+            ),
+            color="secondary"
+        ))),
+        dbc.Row(
+            [
+                dbc.Col(dbc.Input(value='', type='text', id='query')),
+                dbc.Col(dbc.Button('Submit', id='submit-button', color="primary", className="mr-1")),
+            ],
+            className="my-3",
+        ),
+    ],
 )
 
-# Initialize LLM chain
-chain = load_chain()
 
-# Initialize chat history
-if 'messages' not in st.session_state:
-    # Start with first message from assistant
-    st.session_state['messages'] = [{"role": "assistant",
-                                     "content": "Hi human! I am your smart AI. How can I help you today?"}]
-
-# Display chat messages from history on app rerun
-# Custom avatar for the assistant, default avatar for user
-for message in st.session_state.messages:
-    if message["role"] == 'assistant':
-        with st.chat_message(message["role"], avatar=company_logo):
-            st.markdown(message["content"])
-    else:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-# Chat logic
-if query := st.chat_input("Ask me anything"):
+@app.callback(
+    Output('messages', 'children'),
+    Input('submit-button', 'n_clicks'),
+    State('query', 'value'),
+    prevent_initial_call=True
+)
+def update_chat(n_clicks, value):
+    global messages
     # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": query})
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(query)
-
-    with st.chat_message("assistant", avatar=company_logo):
-        message_placeholder = st.empty()
-        # Send user's question to our chain
-        result = chain({"question": query})
-        response = result['answer']
-        full_response = ""
-
-        # Simulate stream of response with milliseconds delay
-        for chunk in response.split():
-            full_response += chunk + " "
-            time.sleep(0.05)
-            # Add a blinking cursor to simulate typing
-            message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
-
+    messages.append({"role": "user", "content": value})
+    # Send user's question to our chain
+    result = chain({"question": value})
+    response = result['answer']
     # Add assistant message to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    messages.append({"role": "assistant", "content": response})
+
+    return [dbc.ListGroupItem(message["content"], color="light", className=message["role"]) for message in messages]
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
